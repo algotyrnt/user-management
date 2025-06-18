ee88a4880b25b317056edd2cd0d0fe061a2a0363
punjitha <punjitha@gmail.com>
2025-06-18 14:11:18 +0530

    init.sql updated

--- backend/resources/database/mysql-init/init.sql
+++ backend/resources/database/mysql-init/init.sql
@@ -5,10 +5,10 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    mobile_number VARCHAR(10),
-    email VARCHAR(255)
+    mobile_number VARCHAR(10) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
 
 INSERT INTO users (first_name, last_name, mobile_number, email) VALUES
-    ("Punjitha", "Bandara", "0772932779", "punjitha@tyrnt.co"),
-    ("Bruce", "Wayne", "0771111111", "bruce@wayne.co");
+    ("Sample", "User1", "0991111111", "sample-user1@user-management"),
+    ("Sample", "User2", "0992222222", "sample-user2@user-management");
