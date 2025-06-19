0c3d47448e0b90f0403f9bca1160cb4c77edf712
punjitha <punjitha@gmail.com>
2025-06-19 07:31:17 +0530

    added user types

---
+++ frontend/src/types/user.tsx
@@ -0,0 +1,14 @@
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    email: string;
+}
+
+export interface newUser {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    email: string;
+}
