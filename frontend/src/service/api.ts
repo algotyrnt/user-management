b7315276452cb88f70aa060fa49e849d30e51e10
punjitha <punjitha@gmail.com>
2025-06-19 07:47:20 +0530

    user api endpoints added

---
+++ frontend/src/service/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import type { newUser, User } from "../types/user";
+
+const api = axios.create({
+    baseURL: "http://localhost:9090/user-management",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+// Users API endpoints
+export const userApi = {
+    // Get user by user id
+    getUserById: (id: number) => api.get<User>(`/users/${id}`),
+
+    // Get all the users
+    getUsers: () => api.get<User[]>(`/users`),
+
+    // Search user by name
+    searchUserByName: (name: string) => api.get<User[]>(`/users/search?name=${name}`),
+
+    // Create a user
+    createUser: (newUser: newUser) => api.post(`/users`, newUser),
+
+    // Update a user
+    updateUser: (updatesUser: User) => api.put(`/users`, updatesUser),
+
+    // Delete user by Id
+    deleteUser: (id: number) => api.delete(`/users/${id}`)
+};
